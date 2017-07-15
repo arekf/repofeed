34cd3fe0fa0f3af988052c2779ddd1b848fbf44e
Arkadiusz Fal <arek@arekf.net>
2017-07-15 17:14:02 +0200

    Add repos controller specs

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'uglifier'
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'database_cleaner'
+  gem 'factory_girl_rails'
+  gem 'pry-rails'
   gem 'rspec-rails'
   gem 'rubocop'
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'database_cleaner'
@@ -5,7 +7,7 @@ require 'database_cleaner'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -57,14 +59,21 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.before(:suite) do
-     DatabaseCleaner.strategy = :transaction
-     DatabaseCleaner.clean_with(:truncation)
-   end
+  config.include FactoryGirl::Syntax::Methods
 
-   config.around(:each) do |example|
-     DatabaseCleaner.cleaning do
-       example.run
-     end
-   end
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      DatabaseCleaner.clean_with(:truncation)
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
